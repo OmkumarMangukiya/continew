f1ab2cf6c5a6f22b2a42513b284d6c100ff8f135
OmkumarMangukiya <ommangukiya8@gmail.com>
2026-08-15 14:34:23 +0530

    Register Endpoint

--- dist/api/server.js
+++ dist/api/server.js
@@ -1,39 +1,31 @@
 import express from "express";
-import { WebhookEndpoint } from "../core/type.js";
-import { Request, Response } from "express";
-import crypto, { randomBytes, sign } from "crypto";
+import crypto from "crypto";
 const app = express();
-
 app.use(express.json());
-
 // temporary using in-memory instead postgres for registered endpoints
-const registeredEndpoints = new Map<string, WebhookEndpoint>();
-
-const handleEndpoint = (req: Request, res: Response) => {
+const registeredEndpoints = new Map();
+const handleEndpoint = (req, res) => {
     const { url } = req.body ?? {};
     if (!url || typeof url !== 'string') {
         return res.status(400).json({ error: "URL is required" });
     }
-    const id = crypto.randomUUID()
+    const id = crypto.randomUUID();
     const signingSecret = crypto.randomBytes(32).toString('hex');
-
-    const endpoint: WebhookEndpoint = {
+    const endpoint = {
         id: id,
         url,
         signingSecret: signingSecret,
         createdAt: new Date(),
         isActive: true
-    }
-
+    };
     registeredEndpoints.set(id, endpoint);
-
     return res.status(201).json({ message: "Endpoint is created", endpoint });
-}
-
+};
 app.post('/endpoints', handleEndpoint);
-app.get('/', ((req: Request, res: Response) => {
-    res.json({ message: "server is running" })
+app.get('/', ((req, res) => {
+    res.json({ message: "server is running" });
 }));
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
-})
+});
+//# sourceMappingURL=server.js.map
---
+++ dist/core/type.js
@@ -0,0 +1,3 @@
+// File to define custom types used in this project
+export {};
+//# sourceMappingURL=type.js.map
