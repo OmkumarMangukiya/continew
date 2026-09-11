b57000020ebf9301789225768a4879d8b75f080f
OmkumarMangukiya <ommangukiya8+github@gmail.com>
2026-09-11 19:23:25 +0530

    SQL Schema: users & api_key

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS endpoints(
     id              TEXT PRIMARY KEY,
+    user_id         TEXT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     url             TEXT NOT NULL,
     signing_secret  TEXT NOT NULL,
     created_at      TIMESTAMPTZ DEFAULT NOW(),
@@ -8,7 +9,7 @@ CREATE TABLE IF NOT EXISTS endpoints(
 
 CREATE TABLE IF NOT EXISTS events(
     id              TEXT PRIMARY KEY,
-    endpoint_id     TEXT NOT NULL REFERENCES endpoints(id),
+    endpoint_id     TEXT NOT NULL REFERENCES endpoints(id) ON DELETE CASCADE,
     type            TEXT NOT NULL,
     payload         JSONB NOT NULL,
     created_at      TIMESTAMPTZ DEFAULT NOW()
@@ -17,10 +18,27 @@ CREATE TABLE IF NOT EXISTS events(
 CREATE TABLE IF NOT EXISTS delivery_attempts(
     id              BIGSERIAL PRIMARY KEY,
     event_id        TEXT NOT NULL REFERENCES events(id) ON DELETE CASCADE,
-    status          TEXT NOT NULL, -- 'pending', 'succeded', 'failed', 'exhausted'
+    status          TEXT NOT NULL, -- 'pending', 'succeeded', 'failed', 'exhausted'
     response_code   INTEGER, -- NULL if request never reached server
     latency_ms      INTEGER,
     error           TEXT,
     attempt_number  INTEGER,
+    created_at      TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS users(
+    id              TEXT PRIMARY KEY,
+    email           VARCHAR(255) UNIQUE NOT NULL,
+    password_hash   VARCHAR(255),
+    username        VARCHAR(50) UNIQUE NOT NULL,
+    created_at      TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS api_keys (
+    id              TEXT PRIMARY KEY,
+    user_id         TEXT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    key_hash        TEXT UNIQUE NOT NULL,
+    name            VARCHAR(100),
+    is_active       BOOLEAN DEFAULT TRUE,
     created_at      TIMESTAMPTZ DEFAULT NOW()
 );
